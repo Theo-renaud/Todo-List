463deb6ceea51eb28e2db4b0a82cf16630ab9466
Lou Valade <lou.valade@etu.uca.fr>
2022-12-05 23:28:40 +0100

    Ajoute le UserController + le Router + le formulaire de connexion

---
+++ vues/ConnexionUtilisateur.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="/vues/style/FormLayout.css">
+<form enctype="multipart/form-data" action="" method="post" id="form">
+<h1>Formulaire de connexion</h1>
+<p>Nom d'utilisateur  <br> <input type="text" name="login" /></p>
+<p>Mot de passe  <br> <input type="password" name="mdp" /></p>
+<p><input type="submit" value="REGISTER"></p>
+</form>
